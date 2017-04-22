db67d8d6971cfe0e86d1a8ceb7be741deb0b2b96
Joao Diogo Costa <jdscosta91@gmail.com>
2017-04-22 11:30:37 +0100

    Move soil.cr file to the correct directory

--- src/soil.cr
+++ src/soil.cr
@@ -2,12 +2,12 @@ require "http/server"
 require "logger"
 require "json"
 
-require "./config"
-require "./http/*"
-require "./routing/*"
-require "./application/*"
-require "./action/*"
-require "./*"
+require "./soil/config"
+require "./soil/http/*"
+require "./soil/routing/*"
+require "./soil/application/*"
+require "./soil/action/*"
+require "./soil/*"
 
 module Soil
   alias Handler = Soil::Action | (Soil::Http::Request, Soil::Http::Response ->)
